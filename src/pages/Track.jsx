4444a8674a8d9da14bdfd6ed900cdcd831563be7
Ghans-1999 <75354247+Ghans-1999@users.noreply.github.com>
2024-07-08 19:10:45 +0530

    Update Track.jsx

--- src/pages/Track.jsx
+++ src/pages/Track.jsx
@@ -1,10 +1,10 @@
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useParams, Link } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 import { Icon } from "leaflet";
 import { MdLocationOn } from "react-icons/md";
 import { useState, useEffect } from "react";
-import { Client, ID, promise, orders } from "../lib/appwrite.js";
+import { Client, ID, promise, orders, databases } from "../lib/appwrite.js";
 
 function Track() {
   const { id } = useParams();
@@ -12,14 +12,30 @@ function Track() {
     latitude: 19.7253,
     longitude: 72.106087,
   });
+  const [parcellatitude, setParcelLatitude] = useState(20.0767253); //19.0767253, 72.9106087
+  const [parcellongitude, setParcelLongitude] = useState(72.9106087);
   const [mylatitude, setMyLatitude] = useState(19.0767253); //19.0767253, 72.9106087
   const [mylongitude, setMyLongitude] = useState(72.9106087);
+  const [mapCenter, setMapCenter] = useState([mylatitude, mylongitude]);
+  const [mapZoom, setMapZoom] = useState(18);
+
+  const CustomerIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png", //https://cdn-icons-png.flaticon.com/128/15746/15746151.png  https://cdn-icons-png.flaticon.com/128/684/684908.png
+    iconSize: [38, 38],
+  });
+
+  const DeliveryIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png", //https://cdn-icons-png.flaticon.com/128/15746/15746151.png  https://cdn-icons-png.flaticon.com/128/684/684908.png
+    iconSize: [38, 38],
+  });
+
+  const [intTime, setIntTime] = useState(0);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        setMyLatitude(position.coords.latitude);
-        setMyLongitude(position.coords.longitude);
+        setMyLatitude(parseFloat(position.coords.latitude));
+        setMyLongitude(parseFloat(position.coords.longitude));
       },
       (error) => {
         console.error(`Error getting location: ${error.message}`);
@@ -32,46 +48,76 @@ function Track() {
       .then((response) => {
         const orders = response.documents;
         const order = orders.find((order) => order.$id === id);
+        console.log(order);
         if (order) {
-          setParcelLocation({
-            latitude: order.parcellattitude,
-            longitude: order.parcellongitude,
-          });
+          setParcelLatitude(parseFloat(order.parcellattitude));
+          setParcelLongitude(parseFloat(order.parcellongitude));
         }
-        console.log(response.documents);
+        console.log(parcellatitude);
+        console.log(parcellongitude);
       })
       .catch((error) => {
         console.error(error);
       });
+    setInterval(() => {
+      const ordersLive = databases.listDocuments(
+        "TastyTreat",
+        "667d7d17002d10cb5826",
+      );
+      ordersLive
+        .then((response) => {
+          const orders = response.documents;
+          const order = orders.find((order) => order.$id === id);
+          console.log(order);
+          if (order) {
+            setParcelLatitude(parseFloat(order.parcellattitude));
+            setParcelLongitude(parseFloat(order.parcellongitude));
+            setIntTime(intTime + 1);
+          }
+          console.log(parcellatitude);
+          console.log(parcellongitude);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }, 30000);
   }, []);
 
-  const CustomIcon = new Icon({
-    iconUrl: "https://cdn-icons-png.flaticon.com/512/5591/5591266.png",
-    iconSize: [38, 38],
-  });
   const markers = [
     {
-      geocode: [mylatitude, mylatitude],
+      geocode: [mylatitude, mylongitude],
       popUp: "Food",
+      icon: CustomerIcon,
+    },
+    {
+      geocode: [parcellatitude, parcellongitude],
+      popUp: `Location : ${parcellatitude} , ${parcellongitude}`,
+      icon: DeliveryIcon,
     },
   ];
 
   return (
     // Make sure you set the height and width of the map container otherwise the map won't show
     <MapContainer
-      center={[mylatitude, mylongitude]}
-      zoom={18}
-      style={{ height: "80%", width: "100%" }}
+      center={mapCenter}
+      zoom={mapZoom}
+      style={{ height: "80vh" }}
+      key={intTime}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[mylatitude, mylongitude]} icon={CustomIcon}></Marker>
+      {/*<Marker position={[mylatitude, mylongitude]} icon={CustomIcon}></Marker>
       <Marker
-        position={[parcelLocation.latitude, parcelLocation.longitude]}
+        position={[parcellatitude, parcellongitude]}
         icon={CustomIcon}
-      ></Marker>
+      ></Marker-->*/}
+      {markers.map((marker, index) => (
+        <Marker position={marker.geocode} icon={marker.geocode.icon}>
+          <Popup>{marker.popUp}</Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 }

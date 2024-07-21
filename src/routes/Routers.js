76151d6a81be9d768f8fc35b63254e9a9d6f5197
Ghans-1999 <75354247+Ghans-1999@users.noreply.github.com>
2024-07-21 19:05:35 +0530

    Update Routers.js

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import Home from "../pages/Home";
 import Pizzas from "../pages/Pizzas";
 import PizzaDetails from "../pages/PizzaDetails";
 import Cart from "../pages/Cart";
 import Checkout from "../pages/Checkout";
-import Collections from "../pages/Collections"
+import Collections from "../pages/Collections";
 import Map from "../pages/MapComponent";
 import PreviousOrders from "../pages/PreviousOrders";
 import Track from "../pages/Track";
+import Login from "../pages/Login";
 
 const Routers = () => {
   return (
@@ -21,10 +22,11 @@ const Routers = () => {
       <Route path="/cart" element={<Cart />} />
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/pizzas/:id" element={<PizzaDetails />} />
-      <Route path="/collection/:category" element={<Collections/>} />
+      <Route path="/collection/:category" element={<Collections />} />
       <Route path="/map" element={<Map />} />
       <Route path="/orders" element={<PreviousOrders />} />
       <Route path="/track/:id" element={<Track />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
